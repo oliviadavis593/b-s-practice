c07e1bc97e6b002a1f90af34276aec67531136e4
Olivia Davis <olivia.davis593@gmail.com>
2020-04-04 15:33:01 -0500

    migration implementation

--- src/store.js
+++ src/store.js
@@ -1,15 +1,16 @@
-const uuid = require('uuid/v4')
+const { v4: uuid4 } = require('uuid')
+uuid4();
 
 const bookmarks = [
     {
-        id: uuid(),
+        id: uuid4(),
         title: 'Google',
         url: 'http://www.google.com',
         description: 'Best search engine on the planet',
         rating: '5'
     },
     {
-        id: uuid(),
+        id: uuid4(),
         title: 'Yelp',
         url: 'http://www.yelp.com',
         description: 'Reviews to inform potential consumers'
